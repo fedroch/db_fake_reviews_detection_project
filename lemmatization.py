a3548ceacfa69c1e6c0036540553366be2c9a11f
robeo <ayvasilev@hse.edu.ru>
2026-04-19 23:57:11 +0300

    не знаю насколько понравиться но вот решил какой то структурности добавить

--- lemmatization.py
+++
@@ -1,7 +0,0 @@
-from nltk.stem import WordNetLemmatizer
-from nltk.tokenize import word_tokenize
-
-def lemmatize_text_no_POS(word: str):
-    '''лемматизация текста(по словам) без указания части речи'''
-    lemmatizer = WordNetLemmatizer()
-    return lemmatizer.lemmatize(word)
