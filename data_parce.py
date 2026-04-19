83a64bcc1a7b0efcf13f81f29ed1057d2f2dcb63
fedroch <fedor.chechik@gmail.com>
2026-04-19 22:30:33 +0300

    ну я немножечко дурачок и перепутал файлы, с кем не бывает

--- data_parce.py
+++ data_parce.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from nltk import word_tokenize
 from sklearn.feature_extraction import text
-from string import punctuation
+from string import punctuation, digits
 
 def print_base_info(df):
     """вывод общей информации о датасете"""
@@ -11,8 +11,6 @@ def print_base_info(df):
     df.info()
     print("describe:")
     df.describe()
-    print('кол-во пропусков в каждом столбце:')
-    print(df.isnull().sum())
     print('первая строка:')
     print(df.head(1))
 
@@ -20,67 +18,25 @@ def print_col(cnt: int, df, rows: list):
     '''вывод первых cnt строк датафрейма из столбцов rows'''
     print(df[rows].head(cnt))
 
-pd.set_option('display.max_columns', None) # добавлено только потому что у меня на экран все не влезает надо не забыть убрать
-raw_data = pd.read_csv('db_fake_job_postings_project/fake_job_postings.csv')
+raw_data = pd.read_csv('db_fake_reviews_detection_project/fake reviews dataset.csv')
 
 clear_data = raw_data
-# print(clear_data.head(1))
 stop_words = text.ENGLISH_STOP_WORDS
-added_stopwords = set(open('db_fake_job_postings_project/stopwords/added_stopwords.txt', 'r').readlines()) # менять можно в файлике
-excluded_stopwords = set(open('db_fake_job_postings_project/stopwords/excluded_stopwords.txt', 'r').readlines()) # менять можно в файлике
+added_stopwords = set(open('db_fake_reviews_detection_project/stopwords/added_stopwords.txt', 'r').readlines()) # менять можно в файлике
+excluded_stopwords = set(open('db_fake_reviews_detection_project/stopwords/excluded_stopwords.txt', 'r').readlines()) # менять можно в файлике
 stop_words = stop_words.union(added_stopwords).difference(excluded_stopwords)
-clear_data['title'] = (
-    clear_data['title']
-    .fillna('')
+clear_data['text_'] = (
+    clear_data['text_']
     .str.lower()
-    .replace('C++', 'cpp').replace('C#', 'Csharp').replace('.NET', 'dotnet')
-    .replace(rf'[{punctuation}\n]', ' ', regex=True)
+    .replace(rf'[{punctuation}{digits}\n]', ' ', regex=True)
     .replace('  ',' ')
     .apply(lambda x: ' '.join([word for word in word_tokenize(x) if word not in stop_words]))
 )
 
-clear_data['company_profile'] = (
-    clear_data['company_profile']
-    .fillna('')
-    .str.lower()
-    .replace('C++', 'cpp').replace('C#', 'Csharp').replace('.NET', 'dotnet')
-    .replace(rf'[{punctuation}\n]', ' ', regex=True)
-    .replace('  ',' ')
-    .apply(lambda x: ' '.join([word for word in word_tokenize(x) if word not in stop_words]))
-)
-
-clear_data['description'] = (
-    clear_data['description']
-    .fillna('')
-    .str.lower()
-    .replace('C++', 'cpp').replace('C#', 'Csharp').replace('.NET', 'dotnet')
-    .replace(rf'[{punctuation}\n]', ' ', regex=True)
-    .replace('  ',' ')
-    .apply(lambda x: ' '.join([word for word in word_tokenize(x) if word not in stop_words]))
-)
-
-clear_data['requirements'] = (
-    clear_data['requirements']
-    .fillna('')
-    .str.lower()
-    .replace('C++', 'cpp').replace('C#', 'Csharp').replace('.NET', 'dotnet')
-    .replace(rf'[{punctuation}\n]', ' ', regex=True)
-    .replace('  ',' ')
-    .apply(lambda x: ' '.join([word for word in word_tokenize(x) if word not in stop_words]))
-)
-
-clear_data['benefits'] = (
-    clear_data['benefits']
-    .fillna('')
-    .str.lower()
-    .replace('C++', 'cpp').replace('C#', 'Csharp').replace('.NET', 'dotnet')
-    .replace(rf'[{punctuation}\n]',' ', regex=True)
-    .replace('  ',' ')
-    .apply( lambda x: ' '.join([word for word in word_tokenize(x) if word not in stop_words]))
-)
 
 # print_base_info(clear_data)
 
 # print('//////////////////////////////////////////////////////////')
 
-# print_col(5, clear_data, ['title', 'company_profile', 'description', 'requirements', 'benefits'])
+# print_col(5, clear_data, ['text_'])
+print_base_info(clear_data)
